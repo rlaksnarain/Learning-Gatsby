8c6836359280b2192697aeaf52f707164c634a5c
rlaks1956 <ram@meritamerica.org>
2022-10-27 20:36:25 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
+
 const IndexPage = () => {
-  return(
+  return (
     <main>
       <Layout pageTitle="Home Page">
-      
-      <p> I am making this using Gatsby Tutorial to learn</p>
+
+        <p> I am making this using Gatsby Tutorial to learn</p>
+        {/* <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        /> */}
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="../images/clifford2.png"
+        />
       </Layout>
     </main>
   )
